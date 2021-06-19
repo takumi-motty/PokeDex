284365314640537c8da9eb73d5735f9f4a0675bf
Takumi Fujimoto <mottypublic@gmail.com>
2021-06-19 16:56:02 +0900

    ファイル整理

--- app/src/main/java/com/example/motty/pokedex/model/JapaneseName.kt
+++
@@ -1,8 +0,0 @@
-package com.example.motty.pokedex.model
-
-import java.io.Serializable
-
-class JapaneseName(
-    var ja: String? = null,
-    var en: String? = null
-): Serializable
--- app/src/main/java/com/example/motty/pokedex/model/JapaneseNameList.kt
+++
@@ -1,7 +0,0 @@
-package com.example.motty.pokedex.model
-
-import java.io.Serializable
-
-class JapaneseNameList (
-    var ja_nameList: List<JapaneseName>? = null
-): Serializable
